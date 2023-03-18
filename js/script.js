778b49e175e8efcfad09a61bc066b8199dffaaef
Javed Omor <114854191+javedomor011@users.noreply.github.com>
2023-03-18 07:40:33 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
+    });
+  });
